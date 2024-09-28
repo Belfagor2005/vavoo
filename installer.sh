e343677f27e18a271f22189e9f6a1546773e6c2f
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-09-28 20:45:35 +0200

    1.31

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/vavoo/main/installer.sh -O - | /bin/sh
 ######### Only This 2 lines to edit with new version ######
-version='1.30'
-changelog='\nMinor Fix'
+version='1.31'
+changelog='\nSet server token'
 ##############################################################
 
 TMPPATH=/tmp/vavoo-main
